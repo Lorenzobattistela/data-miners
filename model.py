eb3cbd808458f53b8640b62b14058d38da9dc9ae
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-23 09:52:23 -0300

    [fix] fixing predictions

--- model.py
+++ model.py
@@ -1,36 +1,45 @@
 import dataset
 import logging
 import datetime
-import pickle
+import dill
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 class Model():
-    def __init__(self, model, date_of_creation) -> None:
+    def __init__(self, model: LogisticRegression, date_of_creation: datetime.date, vectorizer: CountVectorizer) -> None:
         self.model = model
         self.date_of_creation = date_of_creation
+        self.vectorizer = vectorizer
 
 def store_model(model: Model, filepath: str = 'model.pkl'):
     try:
         with open(file=filepath, mode="wb") as f:
-            pickle.dump(model, f)
+            dill.dump(model, f)
     except IOError as e:
         logging.error(f"IOError: {e.with_traceback}")
 
-def load_model(filepath: str = 'model.pickle') -> Model:
+def load_model(filepath: str = 'model.pkl') -> Model:
     try:
         with open(file=filepath, mode="rb") as f:
-            model = pickle.load(f)
+            model = dill.load(f)
         return model
     except IOError as e:
         logging.error(f"IOError: {e.with_traceback}")
+
+
+def predict_label(prompt: str, classifier: LogisticRegression, vectorizer: CountVectorizer):
+    # Preprocess input string using the same steps as training data
+    input_vec = vectorizer.transform([prompt])
+
+    # Predict label using trained classifier
+    pred = classifier.predict(input_vec)
+    return 'spam' if pred[0] == 0 else 'ham'
 
 def train_model():
     df = dataset.get_general_dataset()
@@ -40,6 +49,7 @@ def train_model():
     y = df['is_spam']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
     vectorizer = CountVectorizer()
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
@@ -54,9 +64,9 @@ def train_model():
     lr_f1_score = f1_score(y_test, lr_pred)
     lr_cm = confusion_matrix(y_test, lr_pred)
     logging.info(f"Accuracy: {lr_accuracy}\nF1 Score: {lr_f1_score}\nConfusion Matrix: {lr_cm}")
-    return lr_classifier
+    return lr_classifier, vectorizer
 
 def re_train_model():
-    trained = train_model()
-    chosen_model = Model(model=trained, date_of_creation=datetime.date.today())
+    trained, vectorizer = train_model()
+    chosen_model = Model(model=trained, date_of_creation=datetime.date.today(), vectorizer=vectorizer)
     store_model(model=chosen_model)
